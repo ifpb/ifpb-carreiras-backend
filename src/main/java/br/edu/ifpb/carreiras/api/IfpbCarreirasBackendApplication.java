a907979455e67333b50a7e691fbac97ec7acc50a
Filipe Rodrigues <rodrigues.filipe@academico.ifpb.edu.br>
2025-01-13 19:50:31 -0300

    Commit inicial

---
+++ src/main/java/br/edu/ifpb/carreiras/api/IfpbCarreirasBackendApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.carreiras.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfpbCarreirasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfpbCarreirasBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifpb/carreiras/api/IfpbCarreirasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.carreiras.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfpbCarreirasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
